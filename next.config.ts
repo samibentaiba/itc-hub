95b7bae408e5c8635dde113d0d8408bf9f966599
Sami bentaiba <140959042+samibentaiba@users.noreply.github.com>
2025-11-07 20:29:15 +0100

    change environment

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     productionBrowserSourceMaps: true,
     env: {
         NEXT_PUBLIC_APP_VERSION: "0.1.0",
-        state: "local" as stateType,
+        state: "production" as stateType,
         repository: "https://github.com/samibentaiba/itc-hub"
     },
 };
